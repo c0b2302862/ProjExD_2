33884b05d0ab571d0dc9bca61b4ef6b9ca6ceedf
OtaniHonoka <c0b2302862@edu.teu.ac.jp>
2024-04-23 17:35:21 +0900

    演習課題

--- ex2-20240423/dodge_bomb.py
+++ ex2-20240423/dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pygame as pg
 import random
+import time
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -44,6 +45,24 @@ def main():
     bd_rct = bd_img.get_rect()
     bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     vx, vy = +5, +5
+    accs = [a for a in range(1, 11)] #演習2
+    bo_img = pg.Surface((WIDTH, HEIGHT)) #演習3
+    pg.draw.rect(bo_img, (0, 0, 0), pg.Rect(0, 0, WIDTH, HEIGHT))
+    bo_img.set_alpha(200)
+    fonto = pg.font.Font(None, 80)
+    txt = fonto.render("Game Over", True, (255, 255, 255))
+    kk_img2 = pg.image.load("fig/8.png")
+    kk_img3 = pg.image.load("fig/8.png")
+    trct = txt.get_rect()
+    trct.center = WIDTH/2, HEIGHT/2
+    kk_rct2 = kk_img2.get_rect()
+    kk_rct2.center = WIDTH/2, HEIGHT/2
+    kk_rct3 = kk_img2.get_rect()
+    kk_rct3.center = WIDTH/2, HEIGHT/2
+
+
+    
+    
 
 
     while True:
@@ -52,7 +71,14 @@ def main():
                 return
             
         if kk_rct.colliderect(bd_rct): #こうかとんと爆弾がぶつかったら　練習4
-            print("Game Over")
+            
+            screen.blit(bo_img, [0, 0])
+            screen.blit(txt, trct)
+            screen.blit(kk_img2, [WIDTH/2, HEIGHT/2])
+            screen.blit(kk_img3, [1000, HEIGHT/2])
+            #print("Game Over")
+            pg.display.update()
+            time.sleep(5)
             return
         
 
@@ -82,11 +108,17 @@ def main():
         #爆弾に移動と表示
         bd_rct.move_ip(vx, vy)
         screen.blit(bd_img, bd_rct)
+        
         yoko, tate = check_bound(bd_rct)
         if not yoko: #横にはみ出たら
             vx *= -1
         if not tate: #縦にはみ出たら
             vy *= -1
+
+        #for r in range(1, 11):
+        #    bb_img = pg.Surface((20*r, 20*r))
+        #    pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
